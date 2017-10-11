cefdac78fb8a68d186fe57e09e674cb42f2ab7be
= <=>
2017-10-11 21:47:11 +0100

    Redesign. Mkaing overall more minimalist

--- script.js
+++ script.js
@@ -13,25 +13,9 @@ function checkVisible(elm) {
 
 
 var initiate = function(){
-	var element1 = document.getElementById('projects-alert');
-	var element2 = document.getElementById('rails-alert');
-	var element3 = document.getElementById('javascript-alert');
-	var element4 = document.getElementById('android-alert');
 	var element5 = document.getElementById('main-content');
 
 	window.addEventListener("scroll", function(){
-		if(checkVisible(element1)){
-			element1.setAttribute("class", "alert-right slide");
-		};
-		if(checkVisible(element2)){
-			element2.setAttribute("class", "alert-left slide-small");
-		};
-		if(checkVisible(element3)){
-			element3.setAttribute("class", "alert-left slide-small");
-		};
-		if(checkVisible(element4)){
-			element4.setAttribute("class", "alert-left slide-small");
-		};	
 		if(checkVisible(element5)){
 			element5.setAttribute("class", "container is-fluid main-container fadecolor");
 		};		
