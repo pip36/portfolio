a5c65d968c17cf2e09787f2b9d1ab4ab5783b419
= <=>
2017-10-11 18:50:56 +0100

    Add background and card animation

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ var initiate = function(){
 	var element2 = document.getElementById('rails-alert');
 	var element3 = document.getElementById('javascript-alert');
 	var element4 = document.getElementById('android-alert');
+	var element5 = document.getElementById('main-content');
 
 	window.addEventListener("scroll", function(){
 		if(checkVisible(element1)){
@@ -30,6 +31,9 @@ var initiate = function(){
 		};
 		if(checkVisible(element4)){
 			element4.setAttribute("class", "alert-left slide-small");
+		};	
+		if(checkVisible(element5)){
+			element5.setAttribute("class", "container is-fluid main-container fadecolor");
 		};		
 	});
 }
