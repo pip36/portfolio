dba2343f5b9a39b313b4623594fdf63677f7357a
= <=>
2017-10-11 15:32:54 +0100

    Refactor and restyle
    -refactor script to vanilla javascript
    -refactor animations to be css3 rather than jquery
    -import bulma library, and work in style change

--- script.js
+++ script.js
@@ -1,59 +1,42 @@
-$(document).ready(function(){
-	$('.alert-main').css("width", "0%");
-	$('.alert-main h1').hide();
-	$('#gh-list-item').hide();
-	$('.list-item').hide();
+var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+        initiate();
+    }
+}, 10);
 
-//on load animations
-	$('.alert-main').animate({
-		"width" : "50%"
-	}, 500, function(){
-		$('.alert-main h1').fadeIn();
-		$('#gh-list-item').fadeIn(function(){
-			$('.list-item').fadeIn(function(){
-				$('.side-links').animate({
-					"margin-right" : "10%"
-				},500);
-			});
-		});
-	});
+function checkVisible(elm) {
+  var rect = elm.getBoundingClientRect();
+  var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+  return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
 
-//alert right animation
-	$('.alert-right').css("width", "0%");
-	$('.alert-right h2').hide();	
-	var pos = $('.alert-right').position();	
-	$(window).scroll(function(){
-		if (pos.top < $(window).height() + $(window).scrollTop()){
-			animated = true;
-		    $('.alert-right').animate({
-				"width" : "50%"
-			}, 500, function(){
-				$('.alert-right h2').fadeIn();
-		
-			});
-	    }
-	});
 
-	$('.alert-left').each(function(){
-		var self = this;
-		$(this).css("width", "0%");
-		$(this).children().hide();
-		var pos = $(this).position();
-		$(window).scroll(function(){
-		if (pos.top < $(window).height() + $(window).scrollTop()){
-			
-			animated = true;
-		    $(self).animate({
-				"width" : "30%"
-			}, 500, function(){
-				$(this).children().fadeIn();
-		
-			});
-	    }
-	});
+var initiate = function(){
+	var element1 = document.getElementById('projects-alert');
+	var element2 = document.getElementById('rails-alert');
+	var element3 = document.getElementById('javascript-alert');
+	var element4 = document.getElementById('android-alert');
 
+	window.addEventListener("scroll", function(){
+		if(checkVisible(element1)){
+			element1.setAttribute("class", "alert-right slide");
+		};
+		if(checkVisible(element2)){
+			element2.setAttribute("class", "alert-left slide-small");
+		};
+		if(checkVisible(element3)){
+			element3.setAttribute("class", "alert-left slide-small");
+		};
+		if(checkVisible(element4)){
+			element4.setAttribute("class", "alert-left slide-small");
+		};		
 	});
-		
+}
+
+
+
 	
-	
-});
+
+
+
